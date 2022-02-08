e231ccb9766e560e8db8e9a233991d7007e8c221
Andy Mercado <76190638+andymerc@users.noreply.github.com>
2022-02-08 13:38:31 -0800

    table with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>This header is coming from App.js, and the table is coming from ReportTable.js</header>
     </div>
   );
 }
