615252fabd54d86fd353a3e3e62c4b37623dac6d
Devanshi670 <devanshi.patel.183@my.csun.edu>
2022-02-09 18:28:45 -0800

    created modal for test settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Button, Modal, Form} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor()
+  {
+    super()
+    this.state={
+      show: false
+    }
+  }
+  handleModal(){
+    this.setState({show:!this.state.show})
+  }
+  render() {
+     return (
+        <div>
+          <Button onClick={() => {this.handleModal()}}> Test</Button>
+          <Modal show= {this.state.show} onHide={() => {this.handleModal()}} >
+            <Modal.Header closeButton>Test Settings</Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="subjectId">
+                  <Form.Label>Subject ID</Form.Label>
+                  <Form.Control type="subjectId" placeholder="" />
+                  <Form.Text className="text-muted">
+                    <></>
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="trials">
+                  <Form.Label>Number of Trials</Form.Label>
+                  <Form.Control type="trials" placeholder="" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="difficulty">
+                  <Form.Label>Difficulty</Form.Label>
+                    <Form.Select>
+                      <option>Easy</option>
+                      <option>Medium</option>
+                      <option>Hard</option>
+                    </Form.Select>
+                </Form.Group>
+             </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={() => {this.handleModal()}}>Close</Button>
+              <Button>Save</Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      );
+    
+  }
 }
+
 
 export default App;
