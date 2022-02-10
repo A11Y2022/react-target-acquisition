98a8c3146e7762d5884ca97bc0a5d97fc79946bb
alejandrocr2539 <76397998+alejandrocr2539@users.noreply.github.com>
2022-02-09 18:19:25 -0800

    app bar in drawer with router

---
+++ src/view/Report.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Report = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Reports Page</h1>
+    </div>
+  );
+};
---
+++ src/view/Test.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Test = () => {
+  return (
+    <div>
+      {" "}
+      <h1 style={{ textAlign: "center" }}>Test Page</h1>
+    </div>
+  );
+};
