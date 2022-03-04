222cc4002dd84f29e02cb8f6e13d6384870c0125
Andy Mercado <76190638+andymerc@users.noreply.github.com>
2022-03-03 20:32:22 -0800

    When test on nav bar is clicked the modal shows up and there is now a button to stop test

--- src/components/testing/Modal.js
+++ src/components/testing/Modal.js
@@ -1,22 +1,42 @@
+<<<<<<< Updated upstream:src/components/testing/Modal.js
 import React from 'react';
 import {Button, Modal, Form} from 'react-bootstrap';
+=======
+import React, {useState} from 'react';
+import {button, Modal, Form} from 'react-bootstrap';
+>>>>>>> Stashed changes:src/components/Modal.js
 
 export default class FittsModal extends React.Component {
   constructor()
   {
     super()
     this.state={
+<<<<<<< Updated upstream:src/components/testing/Modal.js
       show: false
     }
   }
   handleModal(){
     this.setState({show:!this.state.show})
   }
+=======
+      show: true
+    }
+  }
+
+  handleModal(){
+    this.setState({show:!this.state.show})
+  }
+
+>>>>>>> Stashed changes:src/components/Modal.js
   render() {
      return (
         <div>
-          <Button onClick={() => {this.handleModal()}}> Test</Button>
-          <Modal show= {this.state.show} onHide={() => {this.handleModal()}} >
+          <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <button class="btn btn-danger me-md-5" type="button"
+            onClick={() => {this.handleModal()}}>Stop Trial</button>
+          </div>
+          <Modal show={this.state.show} onHide={() => {this.handleModal()}} >
+
             <Modal.Header closeButton>Test Settings</Modal.Header>
             <Modal.Body>
               <Form>
@@ -44,8 +64,13 @@ export default class FittsModal extends React.Component {
              </Form>
             </Modal.Body>
             <Modal.Footer>
+<<<<<<< Updated upstream:src/components/testing/Modal.js
               <Button onClick={() => {this.handleModal()}}>Close</Button>
               <Button>Save</Button>
+=======
+              {/* <Button onClick={() => {this.handleModal()}}>Close</Button> */}
+              <button type="button" class="btn btn-primary">Start</button>
+>>>>>>> Stashed changes:src/components/Modal.js
             </Modal.Footer>
           </Modal>
         </div>
