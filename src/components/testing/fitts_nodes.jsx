8d5b2826b61437f3fc5fc04c23683b1951c62f4a
alejandrocr2539 <76397998+alejandrocr2539@users.noreply.github.com>
2022-03-29 11:21:35 -0700

    added functional timer that starts when user clicks on the first node and stops when the trial ends

--- src/components/testing/fitts_nodes.jsx
+++ src/components/testing/fitts_nodes.jsx
@@ -1,43 +1,84 @@
-import React, { useEffect, useState } from 'react';
-import ReactFlow from 'react-flow-renderer';
-import { nodeTypes } from './CircleNode';
+import React, { useEffect, useState } from "react";
+import ReactFlow from "react-flow-renderer";
+import Timer from "../timer/Timer";
+import { nodeTypes } from "./CircleNode";
 
 const UpdateNode = ({ initialElements }) => {
   // State Management
   const [elements, setElements] = useState(initialElements);
-  const [nodeId, setNodeId] = useState('1');
-  const nodeBg = 'red';
+  const [nodeId, setNodeId] = useState("1");
+  const nodeBg = "red";
+
+  // Timer State Management
+  const [isActive, setIsActive] = useState(false);
+  const [isStopped, setIsStopped] = useState(true);
+  const [time, setTime] = useState(0);
+  // Timer Values
+  let centiseconds = ("0" + (Math.floor(time / 10) % 100)).slice(-2);
+  let seconds = ("0" + (Math.floor(time / 1000) % 60)).slice(-2);
+  let minutes = ("0" + (Math.floor(time / 60000) % 60)).slice(-2);
 
   // Event Listeners
   const onElementClick = (event, element) => {
-    const nextId = (parseInt(element.id) + 1).toString() //increment integer value and convert to string
+    const nextId = (parseInt(element.id) + 1).toString(); //increment integer value and convert to string
     setNodeId(nextId);
-  }
+    if (element.id === "1") {
+      console.log(
+        element.id + " " + minutes + ":" + seconds + ":" + centiseconds
+      );
+      setTime(0);
+      setIsActive(true);
+      setIsStopped(false);
+    } else {
+      console.log(
+        element.id + " " + minutes + ":" + seconds + ":" + centiseconds
+      );
+    }
+    if (element.id === "7") {
+      setIsActive(false);
+      setIsStopped(true);
+    }
+  };
 
   useEffect(() => {
     setElements((els) =>
       els.map((el) => {
         if (el.id === nodeId) {
-          el.data = { backgroundColor: nodeBg }
-        }
-        else {
-          el.data = { backgroundColor: "black" }
+          el.data = { backgroundColor: nodeBg };
+        } else {
+          el.data = { backgroundColor: "black" };
         }
         return el;
       })
     );
   }, [nodeBg, nodeId, setElements]);
 
+  // Timer useEffect
+  useEffect(() => {
+    let interval = null;
+    if (isActive && isStopped === false) {
+      interval = setInterval(() => {
+        setTime((time) => time + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, isStopped]);
+
   return (
     <div style={{ height: 800, width: 1000 }}>
+      <Timer minutes={minutes} seconds={seconds} centiseconds={centiseconds} />
       <ReactFlow
         elements={elements}
         nodeTypes={nodeTypes}
         onElementClick={onElementClick}
         defaultZoom={1.5}
         minZoom={0.2}
-        maxZoom={4}>
-      </ReactFlow>
+        maxZoom={4}
+      ></ReactFlow>
     </div>
   );
 };
---
+++ src/components/timer/Timer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function Timer({ minutes, seconds, centiseconds }) {
+  return (
+    <div className="timer">
+      <Button variant="dark" size="lg">
+        {minutes}
+      </Button>
+      {" : "}
+      <Button variant="dark" size="lg">
+        {seconds}
+      </Button>
+      {" : "}
+      <Button variant="dark" size="lg">
+        {centiseconds}
+      </Button>
+    </div>
+  );
+}
+
+export default Timer;
