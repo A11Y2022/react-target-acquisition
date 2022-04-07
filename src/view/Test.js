eabdf2f822a83f1177197171b9c36acbe2145375
Andy Mercado <76190638+andymerc@users.noreply.github.com>
2022-04-07 11:17:44 -0700

    this will console log the mouse coordinates on click

--- src/view/Test.js
+++ src/view/Test.js
@@ -1,15 +1,36 @@
-import React from "react";
-import FittsModal from "../components/testing/Modal.js"
-import UpdateNode from '../components/testing/fitts_nodes';
-import { initialElements } from '../components/testing/initialElements';
+import React, { useEffect, useState } from "react";
+import FittsModal from "../components/testing/Modal.js";
+import UpdateNode from "../components/testing/fitts_nodes";
+import { initialElements } from "../components/testing/initialElements";
 
-export const Test = () => {
+const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+
+  const updateMousePosition = (ev) => {
+    setMousePosition({ x: ev.clientX, y: ev.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", updateMousePosition);
+
+    return () => window.removeEventListener("click", updateMousePosition);
+  }, []);
+
+  return mousePosition;
+};
+
+export function Test() {
+  const { x, y } = useMousePosition();
+  const hasMovedCursor = typeof x === "number" && typeof y === "number";
+  
+
   return (
     <div>
       {" "}
       <h1 style={{ textAlign: "center" }}>Test Page</h1>
-      <FittsModal/>
-      <UpdateNode initialElements={initialElements}/>
+      {hasMovedCursor ? console.log(x, y) : ""}
+      <FittsModal />
+      <UpdateNode initialElements={initialElements} />
     </div>
   );
-};
+}
